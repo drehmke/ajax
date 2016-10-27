f1fd5b9307808090e643a503b9174de2c026c52e
Donna Ryan <donna@nerdrose.com>
2016-10-27 16:10:36 -0700

    Adding filters and formating

--- src/ajax/wwwroot/ngApp/controllers/controllers.ts
+++ src/ajax/wwwroot/ngApp/controllers/controllers.ts
@@ -4,11 +4,23 @@ namespace ajax.Controllers {
         public products;
 
         constructor(TaxService) {
-            this.products = [{
-                name: `Milk`,
-                price: 1.55,
-                tax: TaxService.CalculateTax(1.99)
-            }];
+            this.products = [
+                {
+                    name: `Milk`,
+                    price: 1.55,
+                    tax: TaxService.CalculateTax(1.99)
+                },
+                {
+                    name: `Sushi`,
+                    price: 9.99,
+                    tax: TaxService.CalculateTax(9.99)
+                },
+                {
+                    name: `Bulk Almonds`,
+                    price: 5.69,
+                    tax: TaxService.CalculateTax(5.69)
+                }
+            ];
         }
 
         //constructor($http: ng.IHttpService) {
